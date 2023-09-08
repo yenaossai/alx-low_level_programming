4cb8b8606f552c898f3ab99a442f22b4f0d94c3a
yenaossai <yenaossai@gmail.com>
2023-09-08 16:37:46 +0000

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - this program  that prints all single digit numbers
- * Return: always 0
+ * main - Entry point
+ * Description: prints char below 10
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-int n;
-for (n = 0; n < 10; n++)
-{
-putchar(a + '0');
-}
-putchar('\n');
-return (0);
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
 }
 
