c3a691e433a0b463f7eba44d17da425c63887644
yenaossai <yenaossai@gmail.com>
2023-10-19 19:30:45 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,14 +5,14 @@
 
 /**
  * main - prints all the elements of a list_t list.
- * @h: pointer to the list_t list.
+ * @h:name of the list.
  * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
 {
 size_t x = 0;
 
-while (h != NULL)
+while (h)
 {
 if (h->str == NULL)
 {
