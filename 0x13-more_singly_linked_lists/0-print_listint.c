40749bbb70ebd84b2cdbc1ae33ea7413cf2b271d
yenaossai <yenaossai@gmail.com>
2023-10-23 19:37:43 +0000

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		num_nodes++;
 		h = h->next;
 	}
+
 	return (num_nodes);
 }
