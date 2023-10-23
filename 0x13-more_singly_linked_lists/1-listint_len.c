ee959335bdac62bbf39bdc8f2b3ecf7a4b729c61
yenaossai <yenaossai@gmail.com>
2023-10-23 20:00:05 +0000

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stddef.h>
 
 /**
  * listint_len - returns the number of elements in a linked listint_t list
@@ -8,13 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t num = 0
+	size_t num_nodes = 0
 
 		while (h)
 		{
-		num++
+		num_nodes++
 		h = h->next;
 		}
 
-	return (num);
+	return (num_nodes);
 }
