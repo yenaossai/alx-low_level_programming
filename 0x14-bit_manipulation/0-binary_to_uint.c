01ab3bab5ea4e6b7d0402b4efa1ccb3e93a37985
yenaossai <yenaossai@gmail.com>
2023-10-26 18:38:05 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,11 +19,11 @@ unsigned int binary_to_uint(const char *b)
 		if (b[a] != '0' && b[a] != '1')
 			return (0);
 	}
-	for (a = 0; b[a] != ;\0'; a++)
+	for (a = 0; b[a] != '\0'; a++)
 	{
 		num <<= 1;
 		if (b[a] == '1')
 			num += 1;
 	}
-	return (num)
+	return (num);
 }
