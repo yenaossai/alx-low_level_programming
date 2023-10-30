4ea3897c42e22ab5f895a7c63c961325087aca91
yenaossai <yenaossai@gmail.com>
2023-10-30 20:48:32 +0000

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,7 +14,7 @@ void print_version(unsigned char *e_ident);
 void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_type(unsigned long int e_entry, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
@@ -23,7 +23,7 @@ void close_elf(int elf);
  * Description: if the file is not an ELF file - exit code 98
  */
 
-void check_elf(unsigned char *e_ident);
+void check_elf(unsigned char *e_ident)
 {
 	int index;
 
